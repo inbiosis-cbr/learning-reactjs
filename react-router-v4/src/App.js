89e89fcd1f49e728b6e08c881c67592b6cc5a958
kkloke <kokkeong.loke@modefair.com>
2017-11-14 16:07:54 +0800

    Pretty much done with learning react-router-v4.

--- react-router-v4/src/App.js
+++ react-router-v4/src/App.js
@@ -1,10 +1,18 @@
+/* Basic header */
+/*
 import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
+*/
 
+////////////////////////////////////////////////////////////
+
+
+/* Basic Example */
+/*
 const Home = () => (
   <div>
     <h2>Home</h2>
@@ -69,3 +77,137 @@ const BasicExample = () => (
   </Router>
 )
 export default BasicExample
+*/
+
+
+////////////////////////////////////////////////////////////
+/* URL Parameters */
+/*
+const ParamsExample = () => (
+  <Router>
+    <div>
+      <h2>Accounts</h2>
+      <ul>
+        <li><Link to="/netflix">Netflix</Link></li>
+        <li><Link to="/zillow-group">Zillow Group</Link></li>
+        <li><Link to="/yahoo">Yahoo</Link></li>
+        <li><Link to="/modus-create">Modus Create</Link></li>
+      </ul>
+
+      <Route path="/:id" component={Child}/>
+    </div>
+  </Router>
+)
+
+const Child = ({ match }) => (
+  <div>
+    <h3>ID: {match.params.id}</h3>
+  </div>
+)
+
+export default ParamsExample
+*/
+
+
+////////////////////////////////////////////////////////////
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom'
+
+////////////////////////////////////////////////////////////
+// 1. Click the public page
+// 2. Click the protected page
+// 3. Log in
+// 4. Click the back button, note the URL each time
+
+const AuthExample = () => (
+  <Router>
+    <div>
+      <AuthButton/>
+      <ul>
+        <li><Link to="/public">Public Page</Link></li>
+        <li><Link to="/protected">Protected Page</Link></li>
+      </ul>
+      <Route path="/public" component={Public}/>
+      <Route path="/login" component={Login}/>
+      <PrivateRoute path="/protected" component={Protected}/>
+    </div>
+  </Router>
+)
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
+
+const AuthButton = withRouter(({ history }) => (
+  fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome! <button onClick={() => {
+        fakeAuth.signout(() => history.push('/'))
+      }}>Sign out</button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  )
+))
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    fakeAuth.isAuthenticated ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
+
+const Public = () => <h3>Public</h3>
+const Protected = () => <h3>Protected</h3>
+
+class Login extends React.Component {
+  state = {
+    redirectToReferrer: false
+  }
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState({ redirectToReferrer: true })
+    })
+  }
+
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    const { redirectToReferrer } = this.state
+    
+    if (redirectToReferrer) {
+      return (
+        <Redirect to={from}/>
+      )
+    }
+    
+    return (
+      <div>
+        <p>You must log in to view the page at {from.pathname}</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    )
+  }
+}
+
+export default AuthExample
