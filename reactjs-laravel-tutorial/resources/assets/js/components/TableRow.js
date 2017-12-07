6fc5296ddc4b11419c1a11c336815a30b043b21b
kkloke <kokkeong.loke@modefair.com>
2017-12-07 15:14:37 +0800

    Update content.

---
+++ reactjs-laravel-tutorial/resources/assets/js/components/TableRow.js
@@ -0,0 +1,32 @@
+// TableRow.js
+
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class TableRow extends Component {
+  render() {
+    return (
+        <tr>
+          <td>
+            {this.props.obj.id}
+          </td>
+          <td>
+            {this.props.obj.name}
+          </td>
+          <td>
+            {this.props.obj.price}
+          </td>
+          <td>
+            <Link to={"edit/"+this.props.obj.id} className="btn btn-primary">Edit</Link>
+          </td>
+          <td>
+            <form onSubmit={this.handleSubmit}>
+              <input type="submit" value="Delete" className="btn btn-danger"/>
+            </form>
+          </td>
+        </tr>
+    );
+  }
+}
+
+export default TableRow;
